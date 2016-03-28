987d404651c10bb237728127a5aecc7b0c30401f
Wararray <you@example.com>
2016-03-28 04:30:45 -0300

    Logic of Events Union

--- AlienDNA/geneG.c
+++ AlienDNA/geneG.c
@@ -5,7 +5,86 @@
 int main()
 {
     
+    //GENE_WORDS_CONSTANTS
+
+    //Habitat
+    const   char   AIR[] = {"AAT"};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+
+    //Phobia
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    
+    //Affinity
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    
+    //Particulars
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+    const   char   [] = {""};
+
+
+    // setCreature(); Capture what creature I want to create
 
 
     return 0;    
 }
+
+
+/*    setCreature(){
+        
+        switch()
+            case 1: //Sweet Water
+                setSweetWater();
+                break;
+            case 2: //Salt Water
+                setSaltWater();
+                break;
+            case 3: //Glacial
+                setGlacial();
+                break;
+            case 4: //Air
+                setAir();
+                break;
+            case 5: //Florest
+                seFlorest();
+                break;
+            case 6: //Swamp
+                setSwamp();
+                break;
+            case 7: //Desert
+                setDesert();
+                break;
+            case 8: //Fire
+                setFire();
+                break;
+
+    }
+
+*/
+
+/*
+    char[] setSweetWater(){
+    
+        // Less Hydrofobic
+        // Or
+        // More Affinity
+        // 
+
+    }
+
+
+*/
